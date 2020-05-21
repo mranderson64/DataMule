117dda0cd2a7dc42e123e21803130d768c919482
MrAnderson64 <mranderson64@live.com>
2020-05-21 20:54:23 +0100

    the definition of insanity is doing the same thing over and over and expecting a different result

--- scrape.py
+++ scrape.py
@@ -5,3 +5,4 @@ from tbselenium.utils import launch_tbb_tor_with_stem
 from tbselenium.tbdriver import TorBrowserDriver
 with TorBrowserDriver("/liam/home/DataMule/tor-browser/") as driver:
     driver.get('https://check.torproject.org')
+
