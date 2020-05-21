4bd7e2ac8326ce4e7eda8f3311a3481627099c0c
MrAnderson64 <mranderson64@live.com>
2020-05-21 22:00:47 +0100

    initial data loop (will probably chnage)

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from tbselenium.tbdriver import TorBrowserDriver
 import tbselenium.common as cm
 from tbselenium.utils import launch_tbb_tor_with_stem
@@ -6,3 +7,15 @@ from tbselenium.tbdriver import TorBrowserDriver
 with TorBrowserDriver("\liam\home\DataMule\tor-browser\\") as driver:
     driver.get('https://check.torproject.org')
 
+with open('urls.json') as json_file:
+    data = json.load(json_file)
+
+dataLen = len(data[0][0])
+i = 0
+
+while i < dataLen
+    print i + ": " + data[0][i]
+    i += 1
+
+class versus:
+    
