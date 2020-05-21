898f4705b5280740999cd9cc829245d559b2a5ec
MrAnderson64 <mranderson64@live.com>
2020-05-21 20:09:38 +0100

    Scraper Setup

---
+++ scrape.py
@@ -0,0 +1,7 @@
+import sys
+from tbselenium.tbdriver import TorBrowserDriver
+import tbselenium.common as cm
+from tbselenium.utils import launch_tbb_tor_with_stem
+from tbselenium.tbdriver import TorBrowserDriver
+with TorBrowserDriver("/home/DataMule/tor-browser/") as driver:
+    driver.get('https://check.torproject.org')
