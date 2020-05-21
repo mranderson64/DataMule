40e3d3967ca95591ef74cb99addae6dd50afb29d
MrAnderson64 <mranderson64@live.com>
2020-05-21 22:25:54 +0100

    ahhhhhhh

--- scrape.py
+++ scrape.py
@@ -4,7 +4,7 @@ from tbselenium.tbdriver import TorBrowserDriver
 import tbselenium.common as cm
 from tbselenium.utils import launch_tbb_tor_with_stem
 from tbselenium.tbdriver import TorBrowserDriver
-with TorBrowserDriver("/liam/home/DataMule/tor-browser/") as driver:
+with TorBrowserDriver("/home/liam/DataMule/tor-browser/") as driver:
     driver.get('https://check.torproject.org')
 
 with open('urls.json') as json_file:
