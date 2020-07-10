0c13e4bc02bb5e4795ad2b17e9a563ee88e06cef
MrAnderson64 <mranderson64@live.com>
2020-07-10 05:37:39 +0100

    versus web scrape code finished

--- scrape.py
+++ scrape.py
@@ -41,9 +41,23 @@ class versus:
             driver.get(driver.url + '&ipp=100')
             pages = driver.find_element(By.XPATH, '/html/body/section[3]/div/div[2]/div[304]/div[2]').get_text(strip=True)
             print(pages)
-            #sequel.execute(sql, val)
-            #mydb.commit()
-        
+            
+            totalPages = 99
+            
+            i = 0
+            
+            while i < totalPages:
+                js = 'var listingData="{";drugTitle=function(c){return document.getElementsByClassName("listings__product")[c].querySelector("table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)").innerText};drugCat=function(c){return document.getElementsByClassName("listings__product")[c].querySelector("table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)").innerText};drugLocation=function(c){return document.getElementsByClassName("listings__product")[c].querySelector("table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2)").innerText};drugStock=function(c){return document.getElementsByClassName("listings__product")[c].querySelector("table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2)").innerText};drugSales=function(c){return document.getElementsByClassName("listings__product")[c].querySelector("table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(2)").innerText};drugPrice=function(c){return parseFloat(document.getElementsByClassName("listings__price")[c].querySelector("table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1) > span:nth-child(1)").innerText)};drugCurrency=function(c){a=document.getElementsByClassName("listings__price")[c].querySelector("table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1) > span:nth-child(1)").innerText;b=a.split(" ");return b[1]};drugUnit=function(c){return document.getElementsByClassName("listings__price")[c].querySelector("table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)").innerText};listing=document.getElementsByClassName("listings__product");for(i=0;i<listing.length;i++){if(i==listing.length-1){last="}"}else{last=","}listingData=listingData+'"drug":{"title":"'+drugTitle(i)+'","cat":"'+drugCat(i)+'","loc":"'+drugLocation(i)+'","stock":"'+drugStock(i)+'","sales":"'+drugSales(i)+'","price":"'+drugPrice(i)+'","currency":"'+drugCurrency(i)+'","unit":"'+drugUnit(i)+'"}'+last};return listingData'
+                json = driver.execute_script(js)
+                pageData = json.load(json)
+                print(pageData)
+                #sql = "INSERT INTO `Drugs`(`Advert title`, `Category`, `Location`, `Stock`, `Sales`, `Price`, `Units`, `Currency`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])"
+                #sequel.execute(sql, val)
+                #mydb.commit()
+                if i == totalPages-1:
+                    break
+                i += 1
+        mydb.close()
 
 nameList = []
 posList = []
