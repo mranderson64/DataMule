a077b0a716030f345050f35e1fccf324e0076786
MrAnderson64 <mranderson64@live.com>
2020-07-10 05:58:45 +0100

    changed url code

--- scrape.py
+++ scrape.py
@@ -38,7 +38,7 @@ class versus:
             driver.find_element(By.XPATH, '/html/body/section[3]/div/div/form/input[1]').send_keys(self.user)
             driver.find_element(By.XPATH, '/html/body/section[3]/div/div/form/input[2]').send_keys(self.passw)
             driver.find_element(By.XPATH, '/html/body/section[3]/div/div/form/input[5]').click()
-            driver.get(driver.url + '&ipp=100')
+            driver.get(driver.current_url + '&ipp=100')
             pages = driver.find_element(By.XPATH, '/html/body/section[3]/div/div[2]/div[304]/div[2]').get_text(strip=True)
             print(pages)
             
