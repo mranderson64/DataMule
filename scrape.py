355e07402d1f5c411690fe3cca9956517d9d6bd4
MrAnderson64 <mranderson64@live.com>
2020-08-25 22:26:28 +0100

    renamed variable jsonD fix str issue

--- scrape.py
+++ scrape.py
@@ -13,9 +13,9 @@ from tbselenium.tbdriver import TorBrowserDriver
 
 with open('/home/liam/DataMule/urls.json') as json_file:
     data = json.load(json_file)
-    
+
 with open('/home/liam/DataMule/config.json') as json_file:
-    config = json.load(json_file)    
+    config = json.load(json_file)
 
 mydb = mysql.connector.connect(
   host=config[0]['host'],
@@ -24,16 +24,16 @@ mydb = mysql.connector.connect(
   database=config[0]['name']
 )
 sequel = mydb.cursor()
-    
+
 class versus:
     def __init__(self, url, user, passw, memw):
         self.url = url
         self.user = user
         self.passw = passw
         self.memw = memw
-        
+
     def scrape(self):
-    
+
         with TorBrowserDriver("/home/liam/DataMule/tor-browser/") as driver:
             driver.get(self.url)
             driver.find_element(By.XPATH, '/html/body/section[3]/div/div/form/input[1]').send_keys(self.user)
@@ -51,19 +51,19 @@ class versus:
             driver.find_element(By.XPATH, '/html/body/section[2]/ul[2]/li[2]/a').click()
             time.sleep(10)
             newURL = driver.current_url + '&ipp=100'
-            driver.execute_script("window.location.href = '"+newURL+"';") #WHY DOES THE TOR MODULE NOT HAVE NAVIGATION 
+            driver.execute_script("window.location.href = '"+newURL+"';") #WHY DOES THE TOR MODULE NOT HAVE NAVIGATION
             time.sleep(10)
             pages = driver.find_element(By.XPATH, '/html/body/section[3]/div/div[2]/div[304]/div[2]').get_attribute('text')
             print(pages)
-            
+
             totalPages = 99
-            
+
             i = 0
-            
+
             while i < totalPages:
                 js = 'var listingData=\"[\";drugTitle=function(c){return document.getElementsByClassName(\"listings__product\")[c].querySelector(\"table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)\").innerText};drugCat=function(c){return document.getElementsByClassName(\"listings__product\")[c].querySelector(\"table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)\").innerText};drugLocation=function(c){return document.getElementsByClassName(\"listings__product\")[c].querySelector(\"table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2)\").innerText};drugStock=function(c){return document.getElementsByClassName(\"listings__product\")[c].querySelector(\"table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2)\").innerText};drugSales=function(c){return document.getElementsByClassName(\"listings__product\")[c].querySelector(\"table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(2)\").innerText};drugPrice=function(c){return parseFloat(document.getElementsByClassName(\"listings__price\")[c].querySelector(\"table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1) > span:nth-child(1)\").innerText)};drugCurrency=function(c){a=document.getElementsByClassName(\"listings__price\")[c].querySelector(\"table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > strong:nth-child(1) > span:nth-child(1)\").innerText;b=a.split(\" \");return b[1]};drugUnit=function(c){return document.getElementsByClassName(\"listings__price\")[c].querySelector(\"table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1)\").innerText};listing=document.getElementsByClassName(\"listings__product\");for(i=0;i<listing.length;i++){if(i==listing.length-1){last=\"]\"}else{last=\",\"}listingData=listingData+\'{\"title\":\"\'+drugTitle(i)+\'\",\"cat\":\"\'+drugCat(i)+\'\",\"loc\":\"\'+drugLocation(i)+\'\",\"stock\":\"\'+drugStock(i)+\'\",\"sales\":\"\'+drugSales(i)+\'\",\"price\":\"\'+drugPrice(i)+\'\",\"currency\":\"\'+drugCurrency(i)+\'\",\"unit\":\"\'+drugUnit(i)+\'\"}\'+last};return listingData'
-                json = driver.execute_script(js)
-                pageData = json.load(json)
+                jsonD = driver.execute_script(js)
+                pageData = json.loads(jsonD)
                 print(pageData)
                 sql = "INSERT INTO `Drugs`(`Advert title`, `Category`, `Location`, `Stock`, `Sales`, `Price`, `Units`, `Currency`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])"
                 sequel.execute(sql, val)
@@ -87,13 +87,13 @@ while i < sites:
         print("ya goofed liam")
     print(str(i) + ": " + str(line))
     nameList.append(line)
-    posList.append(i)    
-    
+    posList.append(i)
+
     if i == sites-1:
         break
     i += 1
-    
-    
+
+
 while a < sites:
     if nameList[a] == "versus":
         pos = posList[a]
@@ -107,4 +107,3 @@ while a < sites:
     if i == sites-1:
         break
     i += 1
-    
