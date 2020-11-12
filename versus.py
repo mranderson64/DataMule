4f9fd05047a6dc74dd0fff582fec50dd6adf6fe7
MrAnderson64 <mranderson64@live.com>
2020-11-12 14:12:32 +0000

    Added Gitnore file

--- versus.py
+++ versus.py
@@ -61,9 +61,13 @@ class versus:
                 jsonD = driver.execute_script(js)
                 pageData = json.loads(jsonD)
                 print(pageData)
-                sql = "INSERT INTO `Drugs`(`Advert title`, `Category`, `Location`, `Stock`, `Sales`, `Price`, `Units`, `Currency`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])"
-                sequel.execute(sql, val)
-                mydb.commit()
+                sql = "INSERT INTO `Drugs`(`Advert title`, `Category`, `Location`, `Stock`, `Sales`, `Price`, `Units`, `Currency`, `Date`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                a = 0
+                while a < len(pageData):
+                    val = (pageData[a]['title'], pageData[a]['cat'], pageData[a]['loc'], pageData[a]['stock'], pageData[a]['sales'], pageData[a]['price'], pageData[a]['unit'], pageData[a]['currency'], datetime.datetime.now().isoformat())
+                    sequel.execute(sql, val)
+                    mydb.commit()
+                    a += 1
                 if i == totalPages-1:
                     break
                 i += 1
